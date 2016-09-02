2b9344382dd1b2a268eb8ccdb7348d54b1b64fb6
David Hayes <dhayes@gmail.com>
2016-09-01 18:48:06 -0600

    Creating a basic plugin file header for the plugin

---
+++ bike-coop-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Bike-Coop Site Plugin
+Description: A plugin for all the Bike Co-op Functionality that shouldn't be in a theme
+Version: 0.1.0
+Author: Code for Fort Collins
+Author URI: http://codeforfoco.org/
+Text Domain: coop-plugin
+Domain Path: /languages
+*/
