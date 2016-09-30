25a93ac0aedc67886f813a5460e79fb547688c25
David Hayes <dhayes@gmail.com>
2016-09-29 19:18:59 -0600

    Very basic saving of data into DB

--- volunteer-legacy-processor.php
+++ volunteer-legacy-processor.php
@@ -24,8 +24,60 @@ function is_email_valid($email)
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
+function create_table_with_wpdb($wpdb)
+{
+    $wpdb->query('CREATE TABLE IF NOT EXISTS `volunteers` (
+          `vol_id` int(10) NOT NULL AUTO_INCREMENT,
+          `status` int(1) NOT NULL,
+          `first_name` longtext,
+          `last_name` longtext,
+          `address` longtext,
+          `city` longtext,
+          `state` longtext,
+          `zip` longtext,
+          `email` longtext,
+          `phone` longtext,
+          `dob` date DEFAULT NULL,
+          `emergency_contact` longtext,
+          `emergency_phone` longtext,
+          `relationship` longtext,
+          `how_learn` longtext,
+          `experience` longtext,
+          `other_skills` longtext,
+          `skill_level` decimal(2,0) DEFAULT NULL,
+          `mechanic` varchar(1) DEFAULT NULL,
+          `recycling` varchar(1) DEFAULT NULL,
+          `bike_retrieval` varchar(1) DEFAULT NULL,
+          `teach` varchar(1) DEFAULT NULL,
+          `organize_shop` varchar(1) DEFAULT NULL,
+          `outreach` varchar(1) DEFAULT NULL,
+          `grant_writing` varchar(1) DEFAULT NULL,
+          `web` varchar(1) DEFAULT NULL,
+          `art` varchar(1) DEFAULT NULL,
+          `handyman` varchar(1) DEFAULT NULL,
+          `expectations` longtext,
+          `comments` longtext,
+          `newsletter` varchar(1) DEFAULT NULL,
+          `recruit` varchar(1) DEFAULT NULL,
+          `events` varchar(1) DEFAULT NULL,
+          `bike_safety` varchar(1) DEFAULT NULL,
+          `representative` varchar(1) DEFAULT NULL,
+          `kid_trips` varchar(1) DEFAULT NULL,
+          `app_date` date DEFAULT NULL,
+          PRIMARY KEY (`vol_id`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=2216 ;'
+    );
+}
+
+function insert_data_give_wpdb_and_data($wpdb, $data)
+{
+    $wpdb->insert('volunteers',
+        $data
+    );
+}
+
 $error_msg = '';
-$error_msg .= "Please fill in the form.<br />";
+
 // check to see if the form has already been successfully submitted
 if (isset ($_POST['volid'])) {
     $error_msg = "You've already successfully submitted your application - thank you<br />";
@@ -167,86 +219,55 @@ if (isset ($_POST['first_name'])) {
 // Google for "SQL injection" and "insecure contact form"
 
 // Do this if no errors were detected AND form has been submitted
-if ($error_msg == '' && isset($_POST['submit'])) {
-    //Close the session
-    // session_write_close();
-    // Connect to the MySQL database
-    // Include our login information
-    include('db_login.php');
-//Connect
-    $dsn = "mysql:host=$db_host;dbname=" . $db_database;
-    $opt = array(
-        // any occurring errors wil be thrown as PDOException
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // an SQL command to execute when connecting
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
-    );
-    $db = new PDO($dsn, $db_username, $db_password, $opt);
-    //Connect
-//	$db_error='There was a problem accessing our system.  Please try again later.';
-//	$connection = @mysql_connect($db_host, $db_username, $db_password) or die($db_error);
-//	// Select the database
-//	$db_select = @mysql_select_db($db_database) or die($db_error);
-    // fix up the date for MySQL
+if ($error_msg == '' && isset($_POST['first_name'])) {
     $birth = strftime("%Y-%m-%d", strtotime($dob));
-//	$birth = str_replace("-","",$birth);
-    $appdate =
-    $query = $db->prepare("INSERT INTO VOLUNTEERS (FirstName, LastName, Address, City, State, 
-		Zip, Email, Phone, DOB, EmergencyContact, EmergencyPhone, Relationship, HowLearn, Experience, 
-		OtherSkills, Level, Greeter, Mechanic, Recycling, BARS, Clean, Handyman, Newsletter, Art, 
-		Fund, Outreach, Events, KidTrips, BikeSafety, Expectations, Comments, AppDate) 
-		VALUES (:fname, :lname, :address, :city, :state, :zip, :email, :phone, :dob, :emergency_contact, 
-		:ephone, :rel, :howlearn, :experience, :otherskills, :skill, :greeter, :mechanic, :recycling, 
-		:bars, :cleaning, :handyman, :newsletter, :art, :fundraising, :community, :events, :kidtrips, 
-		:safety, :expectations, :concerns, CURDATE())");
-    $query->bindValue(':fname', $firstname, PDO::PARAM_STR);
-    $query->bindValue(':lname', $lastname, PDO::PARAM_STR);
-    $query->bindValue(':address', $address, PDO::PARAM_STR);
-    $query->bindValue(':city', $city, PDO::PARAM_STR);
-    $query->bindValue(':state', $state, PDO::PARAM_STR);
-    $query->bindValue(':zip', $zip, PDO::PARAM_STR);
-    $query->bindValue(':email', $email, PDO::PARAM_STR);
-    $query->bindValue(':phone', $phone, PDO::PARAM_STR);
-    $query->bindValue(':dob', $birth, PDO::PARAM_STR);
-    $query->bindValue(':emergency_contact', $emergency_contact, PDO::PARAM_STR);
-    $query->bindValue(':ephone', $emergency_phone, PDO::PARAM_STR);
-    $query->bindValue(':rel', $relationship, PDO::PARAM_STR);
-    $query->bindValue(':howlearn', $how_learn, PDO::PARAM_STR);
-    $query->bindValue(':experience', $experience, PDO::PARAM_STR);
-    $query->bindValue(':otherskills', $other_skills, PDO::PARAM_STR);
-    $query->bindValue(':skill', $skill_level, PDO::PARAM_STR);
-    $query->bindValue(':greeter', $greeter, PDO::PARAM_STR);
-    $query->bindValue(':mechanic', $mechanic, PDO::PARAM_STR);
-    $query->bindValue(':recycling', $recycling, PDO::PARAM_STR);
-    $query->bindValue(':bars', $bars, PDO::PARAM_STR);
-    $query->bindValue(':cleaning', $cleaning, PDO::PARAM_STR);
-    $query->bindValue(':handyman', $handyman, PDO::PARAM_STR);
-    $query->bindValue(':newsletter', $newsletter, PDO::PARAM_STR);
-    $query->bindValue(':art', $art, PDO::PARAM_STR);
-    $query->bindValue(':fundraising', $fundraising, PDO::PARAM_STR);
-    $query->bindValue(':community', $community, PDO::PARAM_STR);
-    $query->bindValue(':events', $local_events, PDO::PARAM_STR);
-    $query->bindValue(':kidtrips', $kidtrips, PDO::PARAM_STR);
-    $query->bindValue(':safety', $safety, PDO::PARAM_STR);
-    $query->bindValue(':expectations', $expectations, PDO::PARAM_STR);
-    $query->bindValue(':concerns', $concerns, PDO::PARAM_STR);
-    try {
-        // run the query
-        $query->execute();
-    } catch (PDOException $e) {
-        echo "The statement failed.\n";
-        echo "getCode: " . $e->getCode() . "\n";
-        echo "getMessage: " . $e->getMessage() . "\n";
-    }
-    // Build our query here and check each variable with mysql_real_escape_string()
+	$birth = str_replace("-","",$birth);
+
+    global $wpdb;
+    create_table_with_wpdb($wpdb);
+    $data = [
+        'first_name' => $firstname,
+        'last_name' => $lastname,
+        'address' => $address,
+        'city' => $city,
+        'state' => $state,
+        'zip' => $zip,
+        'email' => $email,
+        'phone' => $phone,
+        'dob' => $birth,
+        'emergency_contact' => $emergency_contact,
+        'emergency_phone' => $emergency_phone,
+        'relationship' => $relationship,
+        'how_learn' => $how_learn,
+        'experience' => $experience,
+        'other_skills' => $other_skills,
+        'skill_level' => $skill_level,
+        // 'greeter' => $greeter,
+        'mechanic' => $mechanic,
+        'recycling' => $recycling,
+        // 'bars' => $bars,
+        // 'cleaning' => $cleaning,
+        'handyman' => $handyman,
+        'newsletter' => $newsletter,
+        'art' => $art,
+        // 'fundraising' => $fundraising,
+        // 'community' => $community,
+        'events' => $local_events,
+        'kid_trips' => $kidtrips,
+        'bike_safety' => $safety,
+        'expectations' => $expectations,
+        // 'concerns' => $concerns,
+        'app_date' => date('Y-m-d'),
+    ];
+    insert_data_give_wpdb_and_data($wpdb, $data);
 
     // No errors were detected.
-    $volid = mysql_insert_id();
+    // $volid = mysql_insert_id();
     // Redirect to confirmation page.
     echo 'Form saved!';
     die;
 }
 
-if (isset($_POST['submit'])) {
+if ($error_msg !== '') {
     echo '<div style="color: red; font-weight:bold; font-style: italic;">The following problems were detected:' . $error_msg . "</div><br>";
 }
